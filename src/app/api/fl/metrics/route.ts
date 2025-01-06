b0f7709217019c758134ddf339363b67a41714fe
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2025-01-06 11:28:07 -0700

    Update route.ts
    lower maxDuration

--- src/app/api/fl/metrics/route.ts
+++ src/app/api/fl/metrics/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 export async function GET(request: Request) {
   try {
